7c6d128cf0e7e9c6d63f9ad72a64990887f99fbe
Ravi <ravi8737@gmail.com>
2023-03-19 12:20:05 +0530

    Adding chrome options

--- src/test/java/org/jenkins/demo/setup/RunThisTest.java
+++ src/test/java/org/jenkins/demo/setup/RunThisTest.java
@@ -3,14 +3,16 @@ package org.jenkins.demo.setup;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class RunThisTest {
-	
+
 	@Test
 	public void openBrowserAndNavigateToGoogle() {
-		WebDriver driver = new ChromeDriver();
+		ChromeOptions chromeOptions = new ChromeOptions();
+		chromeOptions.addArguments("--remote-allow-origins=*");
+		WebDriver driver = new ChromeDriver(chromeOptions);
 		driver.get("https://www.google.com/");
 	}
-	
 
 }
